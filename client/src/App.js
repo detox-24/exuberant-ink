19ff6ca67595ee45a103a21b2d15c72c54b8bef3
sathiya <sathiyamoorthithuran@gmail.com>
2024-10-19 07:42:29 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+      <h1>The Write Side Up</h1>
+        <nav>
+          <a href = "">Subscribe</a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
