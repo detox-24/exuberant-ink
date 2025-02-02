4a59c969a6f6b4274030c22e6e0899d19b425423
sathiya <sathiyamoorthithuran@gmail.com>
2025-02-02 10:51:45 +0530

    Signin api and jsx

--- gapi/controllers/auth-controller.js
+++ gapi/controllers/auth-controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user-mod.js"
 import bcryptjs from 'bcryptjs'
 import { errHandler } from "../utils/error.js"
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res,next) => {
     const {username, email, passwd} = req.body
@@ -24,4 +25,33 @@ export const signup = async (req,res,next) => {
     }
 
     
+}
+export const signin = async (req,res,next) => {
+    const {email, passwd} = req.body
+
+    if(!email || !passwd || email === '' || passwd === ''){
+        return next(errHandler(400, 'Fill up mandatory fields'))
+    }
+
+    try{
+        const user = await User.findOne({email})
+        if(!user){
+            return next(errHandler(404, 'User not found!'))
+        }
+
+        const isMatch = bcryptjs.compareSync(passwd, user.passwd)
+        if(!isMatch){
+            return next(errHandler(401, 'Invalid credentials!'))
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+        const {passwd: userPasswd, ...rest} = user._doc
+
+        res.status(200).cookie('token', token, {
+            httpOnly: true,}
+        ).json(rest)
+    }catch(error){
+        next(error)
+    }
 }
--- gapi/index.js
+++ gapi/index.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './route/user-route.js'
 import authRoute from './route/auth-route.js'
-
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -20,7 +19,7 @@ app.listen(2324,() => {
     console.log('Server is running on port 2324');
 }); 
 app.use('/api/user',userRoute);
-app.use('/api/auth',authRoute)
+app.use('/api/auth',authRoute);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
